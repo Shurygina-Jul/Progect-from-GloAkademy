3d7343d35dbc559b5da95b5ad12128ef3be5f72d
Shurygina-Jul <melam.2916@gmail.com>
2020-08-20 13:49:19 +0300

    day 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 new WOW().init();
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".progect-pagination",
+    bulletClass: ".progect-bullet",
+    bulletActiveClass: ".progect-bullet-active",
+    clickable: true,
+  },
+});
