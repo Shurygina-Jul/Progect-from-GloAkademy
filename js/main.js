0fe059ca3c37fd4fb01d8abba3c1c732c1fceebd
Shurygina-Jul <melam.2916@gmail.com>
2020-08-21 08:56:29 +0300

    fix day2+day3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 new WOW().init();
 var mySwiper = new Swiper(".swiper-container", {
   pagination: {
-    el: ".progect-pagination",
-    bulletClass: ".progect-bullet",
-    bulletActiveClass: ".progect-bullet-active",
+    el: ".progects-pagination",
+    bulletClass: "progects-bullet",
+    bulletActiveClass: "progects-bullet-active",
     clickable: true,
   },
 });
